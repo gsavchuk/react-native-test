6f972b53ca2d50f66cc9e7deb540cf9ad5a6ebc3
Gennadiy Savchuk <gennady.savchuk@gmail.com>
2017-07-02 17:44:03 +0300

    Add basic login form components.

--- App.js
+++ App.js
@@ -1,12 +1,31 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+
+function LabelledInput(props) {
+  return (
+    <View>
+      <Text>{props.label}</Text>
+      <TextInput placeholder={props.placeholder} secureTextEntry={props.obscured} />
+    </View>
+  )
+}
+
+class LoginForm extends React.Component {
+  render() {
+    return (
+      <View>
+        <LabelledInput label="Username:" placeholder="john" />
+        <LabelledInput label="Password:" placeholder="mypass" obscured />
+        <Button title="Login" />
+      </View>
+    )
+  }
+}
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>test</Text>
-      </View>
+      <LoginForm />
     );
   }
 }
